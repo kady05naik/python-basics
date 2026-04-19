62046b1b18f94047c927ac6f5be3f6173fab6fd1
kady05naik <kady05naik@gmail.com>
2026-04-19 18:07:31 +0530

    first commit

---
+++ escape_sequences.py
@@ -0,0 +1,15 @@
+# print --> hello "python" with escape sequence
+print("hello \"python\"")
+# print --> hello "python" without escape sequence
+print('hello "python"')   
+
+
+# print --> hello 'python'  with escape sequence
+print('hello \'python\'')
+# print --> hello 'python' without escape sequence
+print("hello 'python'")
+
+
+# print path  with escape sequence
+print('C://Kadambari/python-basics')
+# print path  without  escape sequence

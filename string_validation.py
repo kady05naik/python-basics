ca4855b8ea75a1b064d328745519a10124bd989f
kady05naik <kady05naik@gmail.com>
2026-04-21 16:16:11 +0530

    Add string_validation.py

---
+++ string_validation.py
@@ -0,0 +1,9 @@
+# Return True if the string contains only alphabets
+str='abdksjkj'
+print(str.isalpha())
+
+# Return True if the string contains only numbers
+str="12542667363E"
+print(str.isnumeric())
+
+# These functions prevents entering garbage data into your system

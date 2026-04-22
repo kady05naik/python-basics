55a6cd91811a2eb086cd49bb5f3c5a9356a1f1f6
kady05naik <kady05naik@gmail.com>
2026-04-22 18:21:28 +0200

    Add

---
+++ numeric_operations/numeric_math_functions.py
@@ -0,0 +1,21 @@
+import math
+
+# Rounding numbers
+price1= 35.5487
+print(round(price1))
+
+price2= 35.487
+print(round(price2))
+
+
+# floor
+print(math.floor(price1))
+print(math.floor(price2))
+
+# ceil
+print(math.ceil(price1))
+print(math.ceil(price2))
+
+# trunc
+print(math.trunc(price1))
+print(math.trunc(price2))
---
+++ numeric_operations/numeric_type.py
@@ -0,0 +1,25 @@
+# Returns the type of variable 
+x=3.14
+print(type(x))
+
+
+# type conversion to int
+integer=11.4
+print(int(integer))
+
+
+# type conversion to float
+float1=22
+print(float(float1))
+
+
+# type conversion to string
+string=35
+print(str(string))
+print(type(str(string)))
+
+
+# type conversion to complex
+x=3
+y=7
+print(complex(x,y))

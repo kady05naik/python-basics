94f25535883c13fe98c5149b88a3cef1e09ba808
kady05naik <kady05naik@gmail.com>
2026-07-18 10:23:30 +0200

    Add

--- collections/list/combining_lists.py
+++ collections/list/combining_lists.py
@@ -14,3 +14,14 @@ numbers=[1,2,3]
 letter.extend(numbers)
 print("extend",letter)
 
+
+letter=['a','b','c']
+num=[1,2,3]
+com=zip(letter,num)
+print(com)
+
+
+letter=['a','b','c']
+num=[1,2,3]
+com=list(zip(letter,num))
+print(com)

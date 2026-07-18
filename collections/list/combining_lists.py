c3a3ee57594e0d3275f3ff67d28ae5cd2b515585
kady05naik <kady05naik@gmail.com>
2026-07-18 09:52:13 +0200

    Add list combining

---
+++ collections/list/combining_lists.py
@@ -0,0 +1,16 @@
+# combining
+letter=['a','b','c']
+numbers=[1,2,3]
+comb=letter+numbers
+print("+",comb)
+
+letter=['a','b','c']
+numbers=[1,2,3]
+comb=letter*2
+print("*",comb)
+
+letter=['a','b','c']
+numbers=[1,2,3]
+letter.extend(numbers)
+print("extend",letter)
+

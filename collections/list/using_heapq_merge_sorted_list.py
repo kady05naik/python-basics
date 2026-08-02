59dc04fea8f295012066975503be3275d6eb6602
kady05naik <kady05naik@gmail.com>
2026-08-02 08:16:43 +0200

    Add

---
+++ collections/list/using_heapq_merge_sorted_list.py
@@ -0,0 +1,11 @@
+import heapq
+
+def headq_merge_two_sorted_lists(list1, list2):
+    return list(heapq.merge(list1, list2))
+
+
+list1=[2,3,4,5,6,7,8]
+list2=[1,4,9]
+print(headq_merge_two_sorted_lists(list1, list2))
+
+
